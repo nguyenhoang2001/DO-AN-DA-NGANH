f9755a83786c6a3eebd129cff5571b18436262e7
minhdang2803 <58287394+minhdang2803@users.noreply.github.com>
2022-03-27 22:11:15 +0700

    New Update - Login

--- smarthome_mobile_app/lib/main.dart
+++ smarthome_mobile_app/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:smarthome_mobile_app/ui/home/home.dart';
+import 'package:smarthome_mobile_app/ui/screens/home.dart';
+import 'theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  ThemeData theme = ThermometerTheme.dark();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomeScreen());
+      navigatorKey: navigatorKey,
+      title: 'Smart Home',
+      theme: theme,
+      home: const HomeScreen(),
+    );
   }
 }
